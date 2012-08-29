bde98f4b83e459ca08c9d9a5c74868eccf96fe6c
unknown <Roel@Roel-HP.(none)>
2012-08-29 17:01:48 +0800

    setup

--- application/modules/sample/controllers/sample.php
+++ application/modules/sample/controllers/sample.php
@@ -2,18 +2,22 @@
 
 class Sample extends MX_Controller
 {
+   private $sModule;
+   
    public function __construct()
    {
       parent::__construct();   
       $this->load->module('core/app');	  
       $this->load->module('site/template');	 
+	  
+	  $this->sModule = strtolower(__CLASS__);
       
    }
    
    public function index()
    {
       $this->template->header();
-	  $this->app->content('sample/v_sample');
+	  $this->app->content($this->sModule.'/v_'.$this->sModule);
 	  $this->template->footer();
 	  
    }
